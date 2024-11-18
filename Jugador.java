6d4523c877ce484e3449c00a516a341804eb5496
Miguel Vila <miviro0804@gmail.com>
2024-11-18 15:39:52 +0100

    logica mal de seleccionar cartas

--- Jugador.java
+++ Jugador.java
@@ -22,8 +22,43 @@ public class Jugador {
         this.mano = mano;
     }
 
-    public Carta jugarCarta() {
+    public Carta jugarCarta(ArrayList<Carta> cartasJugadas, Carta.Palo triunfo) {
+        ArrayList<Carta> cartasJugables = getCartasJugables(mano, triunfo);
+
         return mano.remove(0); // TODO: Implementar lógica de juego
+    }
+
+    private ArrayList<Carta> getCartasJugables(ArrayList<Carta> cartasJugadas, Carta.Palo triunfo) {
+        ArrayList<Carta> cartasJugables = new ArrayList<Carta>();
+        // si no hay cartas jugadas (somos el primer jugador), podemos jugar cualquier
+        // carta
+        if (cartasJugadas.size() == 0) {
+            return mano;
+        }
+
+        // conseguir palo que manda
+        Carta.Palo manda = cartasJugadas.get(0).getPalo();
+        // podemos jugar todas mayores que mayor carta que sea del palo que manda
+        for (Carta carta : mano) {
+            if (carta.getPalo() == manda) {
+                cartasJugables.add(carta);
+            }
+        }
+
+        // si no tenemos cartas del palo que manda
+        if (cartasJugables.size() == 0) {
+            // añadimos los triunfos
+            for (Carta carta : mano) {
+                if (carta.getPalo() == triunfo) {
+                    cartasJugables.add(carta);
+                }
+            }
+            // si no tenemos triunfos, podemos jugar cualquier carta
+            if (cartasJugables.size() == 0) {
+                return mano;
+            }
+        }
+        return cartasJugables;
     }
 
     public void recibirCarta(Carta carta) {

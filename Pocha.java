943907eb2e79c9576997bab60368b607aa126156
Miguel Vila <miviro0804@gmail.com>
2024-12-05 16:11:06 +0100

    3.5 millones

--- Pocha.java
+++ Pocha.java
@@ -1,7 +1,7 @@
 public class Pocha {
     public static GeneradorRL generador = new GeneradorRL();
 
-    public static final int NUM_PARTIDAS = 1000000;
+    public static final int NUM_PARTIDAS = 10000000;
     public static float learning_rate = 0.1f;
 
     public static int currentPartida = 0;
